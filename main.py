8c1a49ff0beb804a2442e9f21e1734253d2fa0b4
irishandyesiam <nathan.irish@gmail.com>
2022-08-05 17:07:04 -0500

    End of the day update.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 # Task 1: As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment in their own separate Lists. 
 
-destinations = ["San Diego", "Chicago", "Stillwater", "Eau Claire"]
+destinations = ["San Diego.", "Chicago.", "Stillwater.", "Eau Claire."]
 restaurants = ["The Pub.", "The Grill.", "The Seashack.", "The Vegan."]
 mode_of_transportation = ["Train.", "Airplane.", "Bicycle.", "Dogsled."]
 entertainment = ["dancing.", "watch a Movie.", "roller skating.", "feeding pigeons."]
@@ -18,36 +18,56 @@ entertainment = ["dancing.", "watch a Movie.", "roller skating.", "feeding pigeo
 rand_number = random.randrange(0,4)
 rand_dest = (destinations[rand_number])
 
-which_dest = input("Would you like me to recommend a destination? y/n")
-for answer in which_dest:
-    if answer == "y":
-        print("Okay,", rand_dest, "is your destination.")
+print("Here is your random destination." , rand_dest, "Does this work for you?")
+
+is_satisfied = False
+while is_satisfied == False:
+    answer = input("yes/no")
+    if answer == "yes":
+        print("Cool. Moving on.")
+        break
+    elif answer == "no":
+        print("Let's try again.", rand_dest)
+    else:
+        print("Please enter yes or no.")
+# while yes_no 
+
+
+
+# while yes_no == "y" or "n":
+#     if yes_no == "y":
+#         print("Cool. Moving on.")
+#         break
+#     elif yes_no == "n":
+#         print("Let's try again.")
+    
+    
    
 
-# restaurant
-rand_number = random.randrange(0,4)
-rand_restr = (restaurants[rand_number])
+# # restaurant
+# rand_number = random.randrange(0,4)
+# rand_restr = (restaurants[rand_number])
 
-which_restr = input("Would you like me to recommend a restaurant? y/n")
-for answer in which_restr:
-    if answer == "y":
-        print("Okay, you will be dining at",rand_restr)
+# which_restr = input("Would you like me to recommend a restaurant? y/n")
+# for answer in which_restr:
+#     if answer == "y":
+#         print("Okay, you will be dining at",rand_restr)
    
 
-# mode of transportation
-rand_number = random.randrange(0,4)
-rand_mod = (mode_of_transportation[rand_number])
+# # mode of transportation
+# rand_number = random.randrange(0,4)
+# rand_mod = (mode_of_transportation[rand_number])
 
-which_mod = input("Would you like me to recommend a mode of transportation? y/n")
-for answer in which_mod:
-    if answer == "y":
-        print("Okay, you will be traveling via", rand_mod)
+# which_mod = input("Would you like me to recommend a mode of transportation? y/n")
+# for answer in which_mod:
+#     if answer == "y":
+#         print("Okay, you will be traveling via", rand_mod)
 
-# entertainment
-rand_number = random.randrange(0,4)
-rand_entr = (entertainment[rand_number])
+# # entertainment
+# rand_number = random.randrange(0,4)
+# rand_entr = (entertainment[rand_number])
 
-which_entr = input("Would you like me to recommend a mode of transportation? y/n")
-for answer in which_entr:
-    if answer == "y":
-        print("Okay, your entertainment will be", rand_entr)
+# which_entr = input("Would you like me to recommend a mode of transportation? y/n")
+# for answer in which_entr:
+#     if answer == "y":
+#         print("Okay, your entertainment will be", rand_entr)
