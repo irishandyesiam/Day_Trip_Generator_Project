082a5d5c9153dcad82dbe7b9d0268cc2db6fc61f
irishandyesiam <nathan.irish@gmail.com>
2022-08-07 13:30:31 -0500

    selected choice list

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ restaurants = ["The Pub.", "The Grill.", "The Seashack.", "The Vegan."]
 mode_of_transportations = ["Train.", "Airplane.", "Bicycle.", "Dogsled."]
 entertainments = ["dancing.", "watch a Movie.", "roller skating.", "feeding pigeons."]
 
+selected_choice = []
+
 # destination 
 def destination():
     is_satisfied = False
     while is_satisfied == False:
-        is_satisfied = False
         rand_number = random.randrange(0,4)
         rand_output = (destinations[rand_number])
         print("Here is your random destination." , rand_output, "Does this work for you?")
         answer = input("yes/no ")
         if answer == "yes":
             print("Cool. Moving on.")
+            selected_choice.append(rand_output)
             break
         elif answer == "no":
             print("Let's try again.")
@@ -26,69 +28,72 @@ destination()
 # print(returned rand_output)
 
 
-# # restaurant
+# restaurant
 
-# def restaurant():
-#     is_satisfied = False
-#     while is_satisfied == False:
-#         is_satisfied = False
-#         rand_number = random.randrange(0,4)
-#         rand_output = (restaurants[rand_number])
-#         print("Here is your random restaurant." , rand_output, "Does this work for you?")
-#         answer = input("yes/no ")
-#         if answer == "yes":
-#             print("Cool. Moving on.")
-#             break
-#         elif answer == "no":
-#             print("Let's try again.")
-#         else:
-#             print("Please enter yes or no.")
-# restaurant()
+def restaurant():
+    is_satisfied = False
+    while is_satisfied == False:
+        is_satisfied = False
+        rand_number = random.randrange(0,4)
+        rand_output = (restaurants[rand_number])
+        print("Here is your random restaurant." , rand_output, "Does this work for you?")
+        answer = input("yes/no ")
+        if answer == "yes":
+            print("Cool. Moving on.")
+            selected_choice.append(rand_output)
+            break
+        elif answer == "no":
+            print("Let's try again.")
+        else:
+            print("Please enter yes or no.")
+restaurant()
 
-# # mode_of_transportations
+# mode_of_transportations
 
-# def mode_of_transportation():
-#     is_satisfied = False
-#     while is_satisfied == False:
-#         is_satisfied = False
-#         rand_number = random.randrange(0,4)
-#         rand_output = (mode_of_transportations[rand_number])
-#         print("Here is your random mode of transportation." , rand_output, "Does this work for you?")
-#         answer = input("yes/no ")
-#         if answer == "yes":
-#             print("Cool. Moving on.")
-#             break
-#         elif answer == "no":
-#             print("Let's try again.")
-#         else:
-#             print("Please enter yes or no.")
-# mode_of_transportation()
+def mode_of_transportation():
+    is_satisfied = False
+    while is_satisfied == False:
+        is_satisfied = False
+        rand_number = random.randrange(0,4)
+        rand_output = (mode_of_transportations[rand_number])
+        print("Here is your random mode of transportation." , rand_output, "Does this work for you?")
+        answer = input("yes/no ")
+        if answer == "yes":
+            print("Cool. Moving on.")
+            selected_choice.append(rand_output)
+            break
+        elif answer == "no":
+            print("Let's try again.")
+        else:
+            print("Please enter yes or no.")
+mode_of_transportation()
 
-# # entertainments
+# entertainments
 
-# def entertainment():
-#     is_satisfied = False
-#     while is_satisfied == False:
-#         is_satisfied = False
-#         rand_number = random.randrange(0,4)
-#         rand_output = (entertainments[rand_number])
-#         print("Here is your random entertainment." , rand_output, "Does this work for you?")
-#         answer = input("yes/no ")
-#         if answer == "yes":
-#             print("Cool. Moving on.")
-#             break
-#         elif answer == "no":
-#             print("Let's try again.")
-#         else:
-#             print("Please enter yes or no.")
-# entertainment()
+def entertainment():
+    is_satisfied = False
+    while is_satisfied == False:
+        is_satisfied = False
+        rand_number = random.randrange(0,4)
+        rand_output = (entertainments[rand_number])
+        print("Here is your random entertainment." , rand_output, "Does this work for you?")
+        answer = input("yes/no ")
+        if answer == "yes":
+            print("Cool. Moving on.")
+            selected_choice.append(rand_output)
+            break
+        elif answer == "no":
+            print("Let's try again.")
+        else:
+            print("Please enter yes or no.")
+entertainment()
 
-# def generated_list():
-#     trips = ["Destination:", "Transportation:", "Restaurant:", "Entertainment:"]
-#     for item in trips:
-#         print(item)
-# generated_list()
-    
+def generated_list():
+    trips = ["Destination:", "Transportation:", "Restaurant:", "Entertainment:"]
+    for item in trips:
+        print(item)
+generated_list()
+print(selected_choice)
    
 
    
